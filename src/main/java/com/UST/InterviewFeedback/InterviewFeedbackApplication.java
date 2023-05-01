9c1a0beece124255579a164d2bad6c5fb142c657
Bareddy Meghana <bareddymeghana@gmail.com>
2023-05-01 14:24:53 +0530

    Initial commit

---
+++ src/main/java/com/UST/InterviewFeedback/InterviewFeedbackApplication.java
@@ -0,0 +1,12 @@
+package com.UST.InterviewFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewFeedbackApplication {
+
+	public static void main(String[] args) {SpringApplication.run(InterviewFeedbackApplication.class, args);
+	}
+
+}
